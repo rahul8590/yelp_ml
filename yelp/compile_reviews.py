bbeeb46e1caeb14827b6cdd3d0dc2abe1b4ceb42
rahul <rahul8590@gmail.com>
2015-03-17 22:37:36 -0400

    horribly slow version of restaurant compile reviews

--- yelp/compile_reviews.py
+++ yelp/compile_reviews.py
@@ -1,7 +1,6 @@
+#Author: Rahul Ram
+#Date: 3/17/15
 
-'''
-Word dictionary building, word ambience. 
-'''
 
 #!/bin/python
 import json
@@ -9,21 +8,51 @@ import unicodedata
 
 
 
-def category(label):
+business_file = '/rahul_extra/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json'
+review_file = '/rahul_extra/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json'
 
-	
 
+def fetch_bid():
+	'''
+	Returns Business ID of all the restaurtants which come under restaurant cateogry
+	'''
+	bid = []
+	fdata = open('./data/restaurant_categories.txt')
+	rest_category = []
+	for names in fdata:
+		rest_category.append(names)
+
+	fbr = open(business_file,'r')
+	for reviews in fbr:
+		jl = json.loads(reviews)
+		j_categories  = map(str,jl['categories'])
+		if set(j_categories) & set(rest_category):
+			bid.append(str(jl['business_id']))
+	return bid
+
+
+#Returns True if the business id refers to restaturant names
+def sanitize(json_line,label):
+	jstr = json_line[label]
+	jnorm = unicodedata.normalize('NFKD', jstr).encode('ascii','ignore')
+	return jnorm
 
 
 def main():
-	fr = open('yelp_academic_dataset_review.json','r')
-	f = open('reviews_only.txt' ,'a')
+	fr = open(review_file,'r')
+	f = open('restaurant_reviews_only.txt' ,'a')
+
+	blist = fetch_bid()
+
+	print "this list of business ids are ",blist
+
 	for line in fr:
 		jl = json.loads(line)
-		jstr = jl['text']
-		jnorm = unicodedata.normalize('NFKD', jstr).encode('ascii','ignore')
-		f.write(jnorm)
-		f.write("\n\n")
+		cur_bid = sanitize(jl,'business_id')
+		if cur_bid in blist:
+			review_text = sanitize(jl, 'text')
+			f.write(review_text)
+			f.write("\n\n")
 	f.close()
 
 if __name__ == '__main__':
