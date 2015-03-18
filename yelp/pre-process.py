3ecf4d57a0653d5fb4655db1b80696255edb4ea6
jshankar <jshankar@beast.c.lucid-clone-850.internal>
2015-03-18 06:29:04 +0000

    add more regex patterns, add donot_process_words

--- yelp/pre-process.py
+++ yelp/pre-process.py
@@ -5,13 +5,32 @@ import re
 ## replace all year by YEAR
 ## take care of wasn't haven't havenot wasnot -> wasn't haven't
 
-money_re = re.compile('|'.join([
-              r'^\$(\d*\.\d{1,2,3)$',
-              r'^\$(\d+)$']))
+money_re  = re.compile('|'.join([
+                        r'\$(\d*\.\d{1,2,3,4})',   ## $.50000, $.34
+                        r'\$(\d+)',               ## $500, $300
+                        r'\$(\d+\.\d{1,2,3,4})']))  #3 $5.33, $3.2.2
+phone_re  = re.compile('|'.join([
+                        r'(\d(\s|-)){0,1}\d{3}(\s|-)\d{3}-\d{4}',             ## 765-413-3419
+                        r'(\d(\s|-)){0,1}\(\d{3}\)(\s|-)\d{3}-\d{4}' ]))      ## (765)-413-3419, (765) 413-3419
+#month_re   = re.compile(r"Jan|Feb|Mar|Apr|Jun|Jul|Aug|Sep|Sept|Oct|Nov|Dec", re.I)
+#weekday_re = re.compile(r"Mon|Tue|Tues|Wed|Thu|Thurs|Fri", re.I)
+weekend_re = re.compile(r"Saturday|Sunday", re.I)
+year_re    = re.compile(r"^(19|20)\d{2}s*")
+num_re     = re.compile(r"^(.|!|\s)*\d+(.|!|\s)*$")
+re_patterns = (money_re, phone_re, weekend_re, year_re, num_re)
+re_repl     = ("MONEY", "PHONE", "WEEKEND", "YEAR", "NUMBER") 
+
+patterns = zip(re_patterns, re_repl)
+
+donot_process_wrds = {"haven't" : 1, "shouldn't" : 1, "won't" : 1, "don't" : 1}
 
 def process(wrd):
-        if money_re.match(wrd):
-            return "MONEY"
+        if wrd in donot_process_wrds:
+            return wrd
+        for re_pattern, repl in patterns:
+                if re_pattern.match(wrd):
+                      wrd = re_pattern.sub(repl, wrd)
+                      break
         new_wrd = []
         prev_ch = False ## was previous char space ?
         for s in wrd:
