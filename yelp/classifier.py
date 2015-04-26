fa6ca3921c9e58b3350000b7c703974f9b5555df
rahul <rahul8590@gmail.com>
2015-04-26 01:57:41 -0400

    adding hyper-parameter results

--- yelp/classifier.py
+++ yelp/classifier.py
@@ -34,23 +34,23 @@ def cross_val(X,y):
   #  ('reg', BayesianRidge())])
   
   #RandomForestClassifier
-  tuned_parameters = {'n_estimators': [2,5,10,15]}
+  #tuned_parameters = {'n_estimators': [2,5,10,15]}
 
   #Logistic Regression
   #tuned_parameters = {'penalty': ['l1','l2'],
   #                    'C': [1.0,2.0,5.0,10.0]}
 
   #SVM
-  #tuned_parameters = {'C': [1.0,2.0,5.0,10.0],
-  #                    'kernel': ['linear','poly'],
-  #                    'probability': [True]}
+  tuned_parameters = {'C': [1.0,2.0,5.0,10.0],
+                      'kernel': ['linear','poly'],
+                      'probability': [True]}
 
   scores = ['accuracy']
 
   for score in scores:
       print("# Tuning hyper-parameters for %s" % score)
       print()
-      clf = GridSearchCV(RandomForestClassifier(), tuned_parameters, cv=5, scoring=score)
+      clf = GridSearchCV(SVC(), tuned_parameters, cv=5, scoring=score)
       clf.fit(X_train, y_train)
       print("Best parameters set found on development set:")
       print()
@@ -126,7 +126,7 @@ if __name__ == '__main__':
 
   
   #Checking for all the words with label == 1
-  flabel = open('label1_all_random_forest.txt','w')
+  flabel = open('label1_all_svm.txt','w')
   for word in entire_dict_feat:
     rand_word_vector = entire_dict_feat[word]
     #print "word is ",word
